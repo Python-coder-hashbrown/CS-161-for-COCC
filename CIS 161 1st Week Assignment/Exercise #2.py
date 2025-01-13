2c019a614bdb294c487b7c2a8b46ab28e178b029
Python-coder-hashbrown <dhirabayashi@cocc.edu>
2025-01-12 16:16:40 -0800

    Add files via upload

---
+++ CIS 161 1st Week Assignment/Exercise #2.py
@@ -0,0 +1,21 @@
+first_name = ("Dante")
+
+current_age = 21
+
+yearly_savings = 1200
+
+decade_me = current_age + 10
+
+average_monthly_savings = 100.00
+
+five_year_savings = 6000
+
+print(f"""Hello {first_name}! You are currently {current_age} years old. 
+In 10 years you will be {decade_me} years old."""
+
+#I did the triple quotation marks because in my terminal it was too long width wise.
+# If you need me to resubmit it but changed to a single quote let me know. I am happy to change it. 
+   
+f""""
+If you save ${yearly_savings} each year, in 5 years you will have saved ${five_year_savings}. 
+Your average monthly savings is ${average_monthly_savings}. """)
