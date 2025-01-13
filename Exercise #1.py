b317680a2481baa25f64dcb58a1bfa565eb25f0f
Python-coder-hashbrown <dhirabayashi@cocc.edu>
2025-01-12 16:13:00 -0800

    Delete Exercise #1.py

--- Exercise #1.py
+++
@@ -1,8 +0,0 @@
-#My girlfriend has a dog, and I'll be using her.
-#I also am an only child which is why I am using her dog as an example
-
-pet_type = "a mixed Portuguese stray"
-
-pet_name = "Laica"
-
-print(f"I have a {pet_type} and her name is {pet_name}.")
